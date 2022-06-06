29d0377335dcca43e90883c123fd261a25e42bf3
Hemant <hemantvcp@gmail.com>
2022-06-06 18:24:45 +0530

    Coroutine

--- app/src/main/java/com/hariom/androidinterviewcode/MainActivity.kt
+++ app/src/main/java/com/hariom/androidinterviewcode/MainActivity.kt
@@ -2,10 +2,33 @@ package com.hariom.androidinterviewcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var count : Int = 1
+    lateinit var textCount : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textCount = findViewById<TextView>(R.id.tvCounterValue)
+        textCount.text = "$count"
+        Log.i("Thread", "thread runs on ${Thread.currentThread().name}")
     }
+
+
+    fun updateCounter(view: View) {
+
+        Log.i("Thread", "thread runs on ${Thread.currentThread().name}")
+        executeLogRunningTask()
+        textCount.text = "${++count}"
+    }
+
+    private fun executeLogRunningTask(){
+        for ( i in 1..100 ){
+            count = i
+        }
+    }
+
 }
